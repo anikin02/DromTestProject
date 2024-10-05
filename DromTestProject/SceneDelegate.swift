ea5ca1330ed31601dc065d9c34f67547c0fc599a
Danil Anikin <mart4r.clearlist@gmail.com>
2024-10-05 21:39:33 +1000

    Updated VIews

--- DromTestProject/SceneDelegate.swift
+++ DromTestProject/SceneDelegate.swift
@@ -17,9 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
     window = UIWindow(windowScene: windowScene)
-    window?.windowScene = windowScene
+    let navigationController = UINavigationController(rootViewController: ViewController())
+    
+    window?.rootViewController = navigationController
     window?.makeKeyAndVisible()
-    window?.rootViewController = ViewController()
   }
 }
 
--- DromTestProject/ViewСontoller/CollectionViewCell.swift
+++ DromTestProject/ViewСontoller/CollectionViewCell.swift
@@ -2,7 +2,34 @@
 //  CollectionViewCell.swift
 //  DromTestProject
 //
-//  Created by Данил Аникин on 05.10.2024.
+//  Created by anikin02 on 05.10.2024.
 //
 
-import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    let imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = .scaleAspectFit
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
